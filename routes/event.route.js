37b51ac919466d5f644f67b0b789fbb334d6325b
Wictorgirardi <wictor.girardi@gmail.com>
2020-02-19 09:11:05 -0300

    correcao nas rotas

--- routes/event.route.js
+++ routes/event.route.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
+var cors = require('cors')
+
+ 
+app.use(cors())
+ 
 
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 const event_controller = require("../controllers/event.controllers.js");
 
 // a simple test url to check that all of our files are communicating correctly.
-router.get("/test", event_controller.test);
-router.post("/create", event_controller.product_create);
-router.get("/:id", event_controller.product_details);
-router.put("/:id/update", event_controller.product_update);
+router.get("/test",cors(), event_controller.test);
+router.post("/create",cors(), event_controller.product_create);
+router.get("/:id",cors(), event_controller.product_details);
+router.put("/:id/update",cors(), event_controller.product_update);
 module.exports = router;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
+var cors = require('cors')
+
+app.use(cors())
 
 
 const user_controller = require("../controllers/user.controller.js");
 
-router.get("/test", user_controller.test);
-router.post("/create", user_controller.user_create);
-router.get("/:id", user_controller.user_details);
-router.put("/:id/update", user_controller.user_update);
+router.get("/test",cors(), user_controller.test);
+router.post("/create",cors(), user_controller.user_create);
+router.get("/:id",cors(), user_controller.user_details);
+router.put("/:id/update",cors(), user_controller.user_update);
 module.exports = router;
